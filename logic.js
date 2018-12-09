7a31c0edbcf811d1ee30585cc641ca1ec4f32c7b
moshe melmaed <moshe.melamed@mail.huji.ac.il>
2018-12-09 05:41:36 +0200

    check list

---
+++ logic.js
@@ -0,0 +1,111 @@
+window.Activity = {};
+
+
+
+
+
+class List extends React.Component {
+    constructor(props) {
+        super(props);
+        this.deletFromList = this.deletFromList.bind(this);
+        this.handeleCheck = this.handeleCheck.bind(this);
+
+
+    }
+
+    generateActivityString(activity) {
+        var new_activity = `${activity.name}`;
+
+        return new_activity;
+    }
+    deletFromList(event) {
+        this.props.deletFromList(event);
+    }
+    handeleCheck(event){
+        console.log(event.target.id)
+
+    }
+    render() {
+        return (
+            <div className="todo" >
+                {this.props.activities.map((activity, i) => <div key={i} className="list" onClick={this.handeleCheck}>
+                <input type="checkbox" ></input><span>{i + 1}. </span >{this.generateActivityString(activity)} <img id={i} className="delete" src="delete-512.png" onClick={this.deletFromList}></img></div>)}
+            </div>
+        );
+    }
+}
+
+class Activities extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            activities: [],
+            done: []
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.deletFromList = this.deletFromList.bind(this);
+        this.deletForGood = this.deletForGood.bind(this);
+
+
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        var new_activity = {
+            name: this.name.value,
+        };
+        this.state.activities.push(new_activity);
+        this.setState({
+            activities: this.state.activities,
+        });
+    }
+    deletFromList(event) {
+        var deletFromList = event.target.id;
+        var newArr = this.state.activities;
+        var addToDone = this.state.activities[deletFromList];
+        var newDone = this.state.done;
+        newDone.push(addToDone);
+        newArr.splice(deletFromList, 1);
+        this.setState({
+            done: newDone,
+            activities: newArr
+        });
+    }
+    deletForGood(event){
+        var deletFromList = event.target.id;
+        var newDone = this.state.done;
+        newDone.splice(deletFromList, 1);
+        this.setState({
+            done: newDone,
+        });
+    }
+
+    render() {
+        return (
+            <div >
+                <div className="header"><img className="headerimg" src="img2.jpg"></img>My check list</div>
+                <div className="lists">
+                    <div>
+                        <form onSubmit={this.handleSubmit} >
+                            <input className="makeNewActivity" ref={input => this.name = input} placeholder="add to list..."></input>
+                        </form>
+                        <List activities={this.state.activities} deletFromList={this.deletFromList} backToList={event}/>
+                    </div>
+                    <div className="second_list">
+                        <div className="done_list"><span>done list</span></div>
+                        <List activities={this.state.done} deletFromList={this.deletForGood} backToList={this.handleSubmit}/>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+function render() {
+    ReactDOM.render(
+        <Activities />,
+        document.getElementById("root")
+    );
+}
+
+render();

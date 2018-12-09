a87a5656f1102fb853b6afea7f7c7fd666fd6160
moshe melmaed <moshe.melamed@mail.huji.ac.il>
2018-12-09 06:38:26 +0200

    added returnToList functio

--- logic.js
+++ logic.js
@@ -8,9 +8,8 @@ class List extends React.Component {
     constructor(props) {
         super(props);
         this.deletFromList = this.deletFromList.bind(this);
-        this.handeleCheck = this.handeleCheck.bind(this);
-
-
+        this.returnToList = this.returnToList.bind(this);
+        this.generateActivityString = this.generateActivityString.bind(this);
     }
 
     generateActivityString(activity) {
@@ -21,15 +20,15 @@ class List extends React.Component {
     deletFromList(event) {
         this.props.deletFromList(event);
     }
-    handeleCheck(event){
-        console.log(event.target.id)
+    returnToList(event){
+        this.props.backToList(event.target.innerText);
 
     }
     render() {
         return (
             <div className="todo" >
-                {this.props.activities.map((activity, i) => <div key={i} className="list" onClick={this.handeleCheck}>
-                <input type="checkbox" ></input><span>{i + 1}. </span >{this.generateActivityString(activity)} <img id={i} className="delete" src="delete-512.png" onClick={this.deletFromList}></img></div>)}
+                {this.props.activities.map((activity, i) => <div key={i} className="list" >
+                <input type="checkbox" ></input><span>{i + 1}. </span ><span onClick={this.returnToList} ref={input => this.name = input}>{this.generateActivityString(activity)} </span><img id={i} className="delete" src="delete-512.png" onClick={this.deletFromList}></img></div>)}
             </div>
         );
     }
@@ -45,10 +44,21 @@ class Activities extends React.Component {
         this.handleSubmit = this.handleSubmit.bind(this);
         this.deletFromList = this.deletFromList.bind(this);
         this.deletForGood = this.deletForGood.bind(this);
+        this.backToList = this.backToList.bind(this);
+
 
 
     }
-
+    backToList(event){
+        console.log(event);
+        var new_activity = {
+            name: event
+        };
+        this.state.activities.push(new_activity);
+        this.setState({
+            activities: this.state.activities,
+        });
+    }
     handleSubmit(event) {
         event.preventDefault();
         var new_activity = {
@@ -93,7 +103,7 @@ class Activities extends React.Component {
                     </div>
                     <div className="second_list">
                         <div className="done_list"><span>done list</span></div>
-                        <List activities={this.state.done} deletFromList={this.deletForGood} backToList={this.handleSubmit}/>
+                        <List activities={this.state.done} deletFromList={this.deletForGood} backToList={this.backToList}/>
                     </div>
                 </div>
             </div>
